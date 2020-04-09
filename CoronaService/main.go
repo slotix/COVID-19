427155422f37146032713bce4074f5fda33c6923
sleeper <sleeper.ok@gmail.com>
2020-04-09 20:14:18 +0200

    initial commit

---
+++ CoronaService/main.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/prprprus/scheduler"
+)
+
+type Config struct {
+	Host string
+}
+
+// HTMLServer represents the web service that serves up HTML
+type HTMLServer struct {
+	server *http.Server
+	wg     sync.WaitGroup
+}
+
+var (
+	payloadFilePath   = "coronaPayload.json"
+	dfkParseAPIServer = flag.String("p", "http://0.0.0.0:8082/v1/serp?api_key=", "DFK API Server address")
+	mongoHost         = flag.String("m", "127.0.0.1", "MongoDB server address")
+	apiKey            = flag.String("a", "15193b29b58de6b74ef8c8040adc6a2692975d26dc3b9198f4d7ed7ae6fc23e8", "DFK API Key")
+	coronaState       []map[string]string
+)
+
+func init() {
+	/* if os.Getenv("CORONA_SERVER") != "" {
+		viper.Set("CORONA_SERVER", os.Getenv("CORONA_SERVER"))
+	} else {
+		viper.Set("CORONA_SERVER", "127.0.0.1:8008")
+	}
+	flag.Parse() */
+}
+
+func main() {
+	serverCfg := Config{
+		Host: "0.0.0.0:8008",
+	}
+	htmlServer := Start(serverCfg)
+	defer htmlServer.Stop()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+
+	fmt.Println("Single process server : shutting down")
+}
+
+func Start(cfg Config) *HTMLServer {
+	flag.Parse()
+
+	_, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	router := mux.NewRouter()
+
+	// Create the HTML Server
+	htmlServer := HTMLServer{
+		server: &http.Server{
+			Addr:           cfg.Host,
+			Handler:        router,
+			MaxHeaderBytes: 1 << 20,
+		},
+	}
+	//liveness check
+	router.HandleFunc("/ping", healthCheckHandler)
+
+	router.HandleFunc("/corona/{cntr}", coronaHandler)
+	router.HandleFunc("/corona", coronaHandler)
+
+	s, err := scheduler.NewScheduler(1000)
+	if err != nil {
+		panic(err) // just example
+	}
+
+	s.Every().Hour(1).Do(updateCoronaStat)
+	s.Every().Do(updateCoronaStat)
+
+	// Add to the WaitGroup for the listener goroutine
+	htmlServer.wg.Add(1)
+
+	go func() {
+		fmt.Printf("\nDataflow Kit Single Process Server : started (HTTP) : Host=%v\n", htmlServer.server.Addr)
+		htmlServer.server.ListenAndServe()
+		htmlServer.wg.Done()
+	}()
+	return &htmlServer
+}
+
+func (htmlServer *HTMLServer) Stop() error {
+	// Create a context to attempt a graceful 5 second shutdown.
+	const timeout = 5 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	fmt.Printf("\nDataflow Kit Single process Server : stopping\n")
+
+	// Attempt the graceful shutdown by closing the listener
+	// and completing all inflight requests
+	if err := htmlServer.server.Shutdown(ctx); err != nil {
+		// Looks like we timed out on the graceful shutdown. Force close.
+		if err := htmlServer.server.Close(); err != nil {
+			fmt.Printf("\nDataflow Kit Single process Server : stopping : Error=%v\n", err)
+			return err
+		}
+	}
+	// Wait for the listener to report that it is closed.
+	htmlServer.wg.Wait()
+	fmt.Printf("\nDataflow Kit Single Process Server : Stopped\n")
+	return nil
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"alive":true}`)
+}
+
+func coronaHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		return
+	}
+
+	if coronaState == nil || len(coronaState) == 0 {
+		fmt.Println("This shouldn't happen")
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	country, ok := vars["cntr"]
+	if !ok {
+		country = "World"
+	}
+
+	covidStatisctic := map[string]string{}
+	for _, covidStatisctic = range coronaState {
+		if strings.ToLower(covidStatisctic["Country_text"]) == strings.ToLower(country) {
+			writeResponse(w, covidStatisctic)
+			return
+		}
+	}
+
+	fmt.Println("Not Found")
+	covidStatisctic = coronaState[0]
+	writeResponse(w, covidStatisctic)
+}
+
+func updateCoronaStat() {
+	payload, err := ioutil.ReadFile(payloadFilePath)
+	if err != nil {
+		fmt.Printf("An error occure during reading payload file: %s", err.Error())
+		return
+	}
+
+	response, err := http.Post(*dfkParseAPIServer+*apiKey, "application/json", bytes.NewReader(payload))
+	if err != nil {
+		fmt.Printf("Post DFK API request failed: %s", err.Error())
+		return
+	}
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Printf("Failed to read respose body: %s", err.Error())
+			return
+		}
+		fmt.Printf("Failed get corona virus state. Server returned: %s", string(body))
+		return
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Failed read response body: %s", err.Error())
+		return
+	}
+	err = json.Unmarshal(body, &coronaState)
+	if err != nil {
+		fmt.Printf("Failed unmarshal response into map: %s", err.Error())
+		return
+	}
+}
+
+func writeResponse(w http.ResponseWriter, covidStatisctic map[string]string) {
+	buff, err := json.Marshal(covidStatisctic)
+	if err != nil {
+		fmt.Printf("Failed to marshal corona: %s", err.Error())
+		http.Error(w, "Failed to retrieve COVID-19 info", http.StatusInternalServerError)
+		return
+	}
+	_, err = io.WriteString(w, string(buff))
+	if err != nil {
+		fmt.Printf("Failed to write response: %s", err.Error())
+		http.Error(w, "Failed to retrieve COVID-19 info", http.StatusInternalServerError)
+		return
+	}
+	return
+}
---
+++ widget/crnwdg.js
@@ -0,0 +1,119 @@
+var CoronaWidget = (function () {
+    var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+
+    function Widget() {
+        this.url = 'http://0.0.0.0:8008/corona';
+        this.ui = {
+            mainContainer: null,
+            country: null,
+            tot_cases: null,
+            new_cases: null,
+            tot_deaths: null,
+            new_deaths: null,
+            tot_recovered: null,
+            active_cases: null
+        };
+
+        this.init();
+    }
+
+    Widget.prototype._updateData = function (e) {
+        e && e.preventDefault();
+        var xhr = new XHR(),
+            cntr = this.ui.country,
+            tot_cases = this.ui.tot_cases,
+            new_cases = this.ui.new_cases,
+            tot_deaths = this.ui.tot_deaths,
+            new_deaths = this.ui.new_deaths,
+            tot_recovered = this.ui.tot_recovered,
+            active_cases = this.ui.active_cases,
+            resp;
+
+        xhr.timeout = 3000;
+
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    resp = JSON.parse(this.responseText);
+                    cntr.innerHTML = resp['Country_text'] === ''? '0': resp['Country_text'];
+                    tot_cases.innerHTML = resp['Total Cases_text'] === '' ? '0' : resp['Total Cases_text'];
+                    new_cases.innerHTML = resp['New Cases_text'] === '' ? '0' : resp['New Cases_text'];
+                    tot_deaths.innerHTML = resp['Total Deaths_text'] === '' ? '0' : resp['Total Deaths_text'];
+                    new_deaths.innerHTML = resp['New Deaths_text'] === '' ? '0' : resp['New Deaths_text'];
+                    tot_recovered.innerHTML = resp['Total Recovered_text'] === '' ? '0' : resp['Total Recovered_text'];
+                    active_cases.innerHTML = resp['Active Cases_text'] === '' ? '0' : resp['Active Cases_text'];
+                } else {
+                    console.log(`Failed to retrieve COVID-19 statisctic. Server returned status ${this.status}: ${this.responseText}`);
+                }
+            }
+        }
+
+        xhr.ontimeout = function () {
+            console.log('Failed to retrieve COVID-19 statisctic. Timeout');
+        }
+
+        xhr.onerror = function (e) {
+            console.log('Failed to retrieve COVID-19 statisctic.');
+        }
+
+        if (cntr.innerHTML !== '') {
+            this.url += '/' + cntr.innerHTML;
+        } 
+        xhr.open('GET', this.url, true);
+        xhr.send();
+    }
+
+    Widget.prototype._initUI = function () {
+        this.ui.mainContainer = document.getElementById('container');
+        this.ui.country = document.getElementById('country');
+        this.ui.tot_cases = document.getElementById('tot-cases');
+        this.ui.new_cases = document.getElementById('new-cases');
+        this.ui.tot_deaths = document.getElementById('tot-deaths');
+        this.ui.new_deaths = document.getElementById('new-deaths');
+        this.ui.tot_recovered = document.getElementById('tot-recover');
+        this.ui.active_cases = document.getElementById('active-cases');
+    }
+
+    Widget.prototype.__initCountry = function () {
+        return new Promise((resolve, reject) => {
+            var xhr = new XHR();
+            xhr.timeout = 3000;
+            xhr.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        resp = JSON.parse(this.responseText);
+                        resolve(resp.country)
+                    } else {
+                        reject(xhr.status)
+                    }
+                }
+
+            }
+            xhr.ontimeout = function () {
+                reject('timeout')
+            }
+
+            xhr.open('GET', 'http://ip-api.com/json', true);
+            xhr.send();
+        });
+
+    }
+
+    Widget.prototype.init = function () {
+        this._initUI();
+        this.__initCountry().then((country) => {
+            if (country == 'United States') {
+                this.ui.country.innerHTML = 'USA';
+            } else {
+                this.ui.country.innerHTML = country;
+            }
+            this._updateData();
+        }).catch((err) => {
+            console.log(err);
+            this._updateData();
+        });
+    }
+    return Widget;
+})();
+
+new CoronaWidget();
